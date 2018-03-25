4c59c7a3957790976e9ed6d4ce558f56976873a7
Daniel Johnson <dajohnson1978@gmail.com>
2018-03-24 23:14:41 -0500

    re-arranging functions, adding spec to main translation functions

--- src/short_hand/core.clj
+++ src/short_hand/core.clj
@@ -36,6 +36,11 @@
       (keyword (str my-namespace ":" my-name))
       (keyword my-name))))
 
+(defn- to-short-name [long-name]
+  (let [[my-ns my-name] (str/split (name long-name) #":")
+        [my-ns my-name] (if-not (nil? my-name) [my-ns my-name] [nil my-ns])]
+    (symbol my-ns my-name)))
+
 (defn to-long-hand [short-hand]
   (let [short-hand (validate ::spec/short-node short-hand)
         body (if (symbol? short-hand) [short-hand] short-hand)
@@ -53,6 +58,23 @@
                         {:attrs attrs :content content})]
     (validate ::spec/long-node node)))
 
+(s/fdef to-long-hand
+        :args (s/cat :short-hand ::spec/short-node)
+        :ret (s/cat :long-hand ::spec/long-node))
+
+(defn to-short-hand [long-hand]
+  (let [{:keys [tag attrs content]} (validate ::spec/long-node long-hand)
+        tag (to-short-name tag)
+        attrs (if (empty? attrs) [] [(reduce-kv #(assoc %1 (to-short-name %2) (unescape %3)) {} attrs)])
+        content (if (empty? content) [] (mapv #(if (string? %) % (to-short-hand %)) content))
+        attrs-and-content (concat attrs content)
+        body (if (empty? attrs-and-content) tag (into [tag] attrs-and-content))]
+    (validate ::spec/short-node body)))
+
+(s/fdef to-short-hand
+        :args (s/cat :long-hand ::spec/long-node)
+        :ret (s/cat :short-hand ::spec/short-node))
+
 (defn- trim-parsing [{:keys [tag attrs content]}]
   (let [content (if-not (empty? content)
                   (mapv #(if (string? %) (str/trim %) (trim-parsing %)) content)
@@ -64,9 +86,12 @@
 
 (defn stream-long-hand [^InputStream stream]
   (->> stream
-      xml/parse
-      trim-parsing
-      (validate ::spec/long-node)))
+       xml/parse
+       trim-parsing
+       (validate ::spec/long-node)))
+
+(defn stream-short-hand [^InputStream stream]
+  (to-short-hand (stream-long-hand stream)))
 
 (defn long-hand-to-stream [long-hand ^OutputStream stream]
   (let [validated (validate ::spec/long-node long-hand)]
@@ -74,53 +99,42 @@
       (binding [*out* w]
         (xml/emit-element validated)))))
 
+(defn short-hand-to-stream [short-hand ^OutputStream stream]
+  (long-hand-to-stream (to-long-hand short-hand) stream))
+
 (defn read-long-hand [^String xml-string]
   (-> xml-string
       .getBytes
       ByteArrayInputStream.
       stream-long-hand))
 
-(defn write-long-hand [long-hand]
-  (let [out-str (ByteArrayOutputStream.)]
-    (long-hand-to-stream long-hand out-str)
-    (.toString out-str)))
-
-(defn spit-long-hand [file-name long-hand]
-  (let [out-str (io/output-stream file-name)]
-    (long-hand-to-stream long-hand out-str)
-    (.toString out-str)))
-
-(defn slurp-long-hand [file-name]
-  (-> file-name
-      io/input-stream
-      stream-long-hand))
-
-(defn- to-short-name [long-name]
-  (let [[my-ns my-name] (str/split (name long-name) #":")
-        [my-ns my-name] (if-not (nil? my-name) [my-ns my-name] [nil my-ns])]
-    (symbol my-ns my-name)))
-
-(defn to-short-hand [long-hand]
-  (let [{:keys [tag attrs content]} (validate ::spec/long-node long-hand)
-        tag (to-short-name tag)
-        attrs (if (empty? attrs) [] [(reduce-kv #(assoc %1 (to-short-name %2) (unescape %3)) {} attrs)])
-        content (if (empty? content) [] (mapv #(if (string? %) % (to-short-hand %)) content))
-        attrs-and-content (concat attrs content)
-        body (if (empty? attrs-and-content) tag (into [tag] attrs-and-content))]
-    (validate ::spec/short-node body)))
-
 (defn read-short-hand [xml-string]
   (-> xml-string
       read-long-hand
       to-short-hand))
+
+(defn write-long-hand [long-hand]
+  (let [out-str (ByteArrayOutputStream.)]
+    (long-hand-to-stream long-hand out-str)
+    (.toString out-str)))
 
 (defn write-short-hand [short-hand]
   (-> short-hand
       to-long-hand
       write-long-hand))
 
+(defn spit-long-hand [file-name long-hand]
+  (let [out-str (io/output-stream file-name)]
+    (long-hand-to-stream long-hand out-str)
+    (.toString out-str)))
+
 (defn spit-short-hand [file-name short-hand]
   (spit-long-hand file-name (to-long-hand short-hand)))
+
+(defn slurp-long-hand [file-name]
+  (-> file-name
+      io/input-stream
+      stream-long-hand))
 
 (defn slurp-short-hand [file-name]
   (to-short-hand (slurp-long-hand file-name)))
